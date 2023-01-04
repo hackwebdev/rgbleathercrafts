6dca7fe7b891088b05c6c8ae5bc6d2ace154a718
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-01-04 23:50:53 +0800

    react-bootstrap setup, header and footer components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Row>
+        <Col className="text-center py-3">
+          Copyright &copy; RGB LeatherCrafts
+        </Col>
+      </Row>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">RGB LeatherCrafts</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/cart">
+              <i className="fas fa-shopping-cart"></i> Cart
+            </Nav.Link>
+            <Nav.Link href="/login">
+              <i className="fas fa-user"></i> Sign In
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
