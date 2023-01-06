077b4c4ae8025a45b7f1943fa63d2bbd295e5e33
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-01-07 01:26:56 +0800

    serving products - backend routes

---
+++ backend/data/products.js
@@ -0,0 +1,69 @@
+const products = [
+  {
+    _id: "1",
+    name: "Leather Training Catch Bag",
+    image: "/images/catch_bag.png",
+    description:
+      "Made from real leather, very durable and it protects your rooster from powerful blows during training.",
+    brand: "TatakRGB",
+    category: "gamefowl-accessories",
+    price: 2500,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Leather Takera",
+    image: "/images/takera.png",
+    description:
+      "Leather made multiple tari sheath with custom design. Portability at the palm of your hand.",
+    brand: "TatakRGB",
+    category: "gaffing-accesories",
+    price: 2000,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Leather Tari Sheath",
+    image: "/images/tari_sheath.png",
+    description:
+      "Single leather sheath for tari blades. Hand stitched with internal lining to prevent your blades from direct contact to leather.",
+    brand: "TatakRGB",
+    category: "gaffing-accessories",
+    price: 100,
+    countInStock: 5,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Custom Leather Tari Case",
+    image: "/images/tari_case.png",
+    description:
+      "Custom made hand crafted made from authentic leather tari case. With fiberglass and comes in different sizes and custom design.",
+    brand: "TatakRGB",
+    category: "gaffing-accesories",
+    price: 7000,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Leather Arm Protector",
+    image: "/images/arm_protector.png",
+    description:
+      "Protect your hands from man fighting roosters during training and conditioning. Made from thick soft leather. Adjustable will fit any arm sizes.",
+    brand: "TatakRGB",
+    category: "gaffing-accesories",
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+];
+
+module.exports = products;
