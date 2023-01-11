ef674a6b70ec38655028ef6f40154cee2e58b210
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-01-12 03:33:42 +0800

    cast error objectid fix

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -21,7 +21,10 @@ router.get(
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+    const id = req.params.id;
+    const trimmed_id = id.trim();
+
+    const product = await Product.findById(trimmed_id);
     if (product) {
       res.json(product);
     } else {
